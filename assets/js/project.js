cef57a4d420dcf2e8e4d0d6380017b67a3647dc9
Vincent McAulay <bwownbwead@hotmail.com>
2016-05-07 18:03:52 +0100

    continuing with desktop styling

--- assets/js/project.js
+++ assets/js/project.js
@@ -2,19 +2,57 @@ $(document).ready(function(){
 
 	$('html').removeClass("no-js");
 
-	enquire.register("screen and (max-width:45em)", {
+	var mqBreakpoints = {
+		'large-mobile': '380px',
+		'phablet': '480px',
+		'small-tablet': '580px',
+		'tablet': '768px',
+		'desktop': '1024px',
+		'wide': '1366px',
+		'widest': '1600px'
+	};
+
+	function moveProductLink__match() {
+
+		var productCtas = $('.js-move--media-list-info'),
+			infoContainers = $('.js-media-list-info'),
+			classesToRemove = 'cta cta--small';
+
+		productCtas.each(function(i, elem) {
+			var $cta = $(elem),
+				$container = infoContainers.eq(i);
+
+			$cta.removeClass(classesToRemove).appendTo($container);
+		});
+	}
+
+	function moveProductLink__unmatch() {
+
+		var productCtas = $('.js-move--media-list-info'),
+			ctaContainers = $('.js-media-list-cta'),
+			classesToAdd = 'cta cta--small';
+
+		productCtas.each(function(i, elem) {
+			var $cta = $(elem),
+				$container = ctaContainers.eq(i);
+
+			$cta.addClass(classesToAdd).appendTo($container);
+		});
+	}
+
+	enquire.register("screen and (min-width:" + mqBreakpoints['desktop'] + ")", {
 
 	    // OPTIONAL
 	    // If supplied, triggered when a media query matches.
 	    match : function() {
-	    	console.log('matched');
+	    	moveProductLink__match();
 	    },      
 	                                
 	    // OPTIONAL
 	    // If supplied, triggered when the media query transitions 
 	    // *from a matched state to an unmatched state*.
 	    unmatch : function() {
-	    
+	    	moveProductLink__unmatch();
 	    },    
 	    
 	    // OPTIONAL
